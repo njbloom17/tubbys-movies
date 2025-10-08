c3798a5c4df5dcd1fb9ef8ee40426f9e17c74ed9
njbloom17 <njbloom33@gmail.com>
2025-10-08 01:14:43 -0700

    fix: hook useeffect to prevent infinite loop

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -37,25 +37,26 @@ export const useTopRatedMovies = (): Movie[] => {
 
 export const useSearchMovies = (movieQuery: string): Movie[] => {
   const [movies, setMovies] = useState<Movie[]>([]);
+  useEffect(() => {
+    const fetchMovies = async (movieQuery: string): Promise<MoviesResult> => {
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1N2I5YTFmZDZkNmMzNWRjNGRjN2RiYThiYzY2NjllMiIsIm5iZiI6MTc1OTM4MzkzNC43NjEsInN1YiI6IjY4ZGUxMTdlZWQ0OTZlODNmNTQwNWI0OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Is1GufhjdB3hiy22pP4tvI---Dp8AdtiH-burqyhdRM",
+        },
+      };
 
-  const fetchMovies = async (movieQuery: string): Promise<MoviesResult> => {
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1N2I5YTFmZDZkNmMzNWRjNGRjN2RiYThiYzY2NjllMiIsIm5iZiI6MTc1OTM4MzkzNC43NjEsInN1YiI6IjY4ZGUxMTdlZWQ0OTZlODNmNTQwNWI0OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Is1GufhjdB3hiy22pP4tvI---Dp8AdtiH-burqyhdRM",
-      },
+      const res = await fetch(
+        `https://api.themoviedb.org/3/search/movie?query=${movieQuery}&include_adult=false&language=en-US&page=1`,
+        options
+      );
+      if (!res.ok) throw new Error(`Error: ${res.status} ${res.statusText}`);
+      return res.json();
     };
+    fetchMovies(movieQuery).then((res) => setMovies(res.results));
+  }, [movieQuery]);
 
-    const res = await fetch(
-      `https://api.themoviedb.org/3/search/movie?query=${movieQuery}&include_adult=false&language=en-US&page=1`,
-      options
-    );
-    if (!res.ok) throw new Error(`Error: ${res.status} ${res.statusText}`);
-    return res.json();
-  };
-
-  fetchMovies(movieQuery).then((res) => setMovies(res.results));
   return movies;
 };
